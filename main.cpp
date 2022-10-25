76317c01832a67f771864a097166a1be1667e215
SadSatellites <116614206+SadSatellites@users.noreply.github.com>
2022-10-24 22:49:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,17 +14,10 @@ int main()
 
     dayType;        // Create day object using default constructor
 
-//    cout << "Set day of the week: " << endl;
-//    cin >> x;
-//    dayType.setDay(x);
     dayType.printDay();     // Prints currently set day
     cout << "\nGetday: " << dayType.getDay() << endl;       // Returns currently set daw as string
     cout << "\nGet Previous Day: " << dayType.previousDay() << endl;        // Returns previous day of the week
     cout << "\nGet Next Day: " << dayType.nextDay() << endl;        // Returns next day of the week
-
-//    cout << "\nHow many days do you want to add?" << endl;
-//    cin >> y;
-//    dayType.addDay(y);
 
     cout << "\n\nSecond Initialization for Monday." << endl;
     SecondInit;     // Create day object initialized on Monday.
